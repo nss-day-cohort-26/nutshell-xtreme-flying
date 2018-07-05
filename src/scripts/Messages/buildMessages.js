ce90d502895d2e837fb3c549e5fabb8430346722
William Kimball <wkkimball043@gmail.com>
2018-07-05 10:58:00 -0500

    commented partially

--- src/scripts/Messages/buildMessages.js
+++ src/scripts/Messages/buildMessages.js
@@ -7,14 +7,15 @@ const editMess = require("./editMessage")
 // const currentUser = require("./currentUser")
 let currentUse = 1;//dont let this stay 
 
+
+//This function creates an article where all of the message functionality goes, such as the messages, and the new message input field. It then executes an ajax call to the list of messages and runs the functions to build each message with a functional edit button.
 const buildMessageArticle = function () {
 
     const $messageArticle = document.createElement("article")
     $messageArticle.id = "message-article";
 
+    //This part creates the article for messages, the input box, and appends them to the DOM
     $('<section>').attr('id', 'message-box').appendTo($messageArticle);
-
-
     let $inputDiv = $("<div>").attr("id", "input-div")
     $('<input>').attr('type', 'text').attr('id', 'message-input').appendTo($inputDiv);
     $("<button>").attr('type', 'button').attr('id', 'message-btn').text("Send Message").appendTo($inputDiv);
@@ -22,15 +23,17 @@ const buildMessageArticle = function () {
 
     $("#friends").append($messageArticle)
 
+    //this calls the function that adds the event listener to the submit button
     subMess();
 
-
+    //this executes an ajax call to the api for all of the messages, and then builds message components with an edit function for each.
     ajax.getField("messages").then(function (messageList) {
 
+        //ajax call returns an array that is then looped through to build each message
         messageList.forEach(element => {
             let mess = document.createElement("p")
 
-
+            //this ajax call gets the userId of the message, which is used to give proper ids to the elements, and will be used to check for current user later on.
             ajax.getUser(element.userId).then(function (response) {
                 mess.textContent = `${response.name}: ${element.message}`
                 let userName = response.name 

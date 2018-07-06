983ff9d85e5747a584ea6fdfc3d0faa652fe518a
Jennifer Lawson <jenniferlawson000@gmail.com>
2018-07-06 13:53:58 -0500

    add friend from chat

--- src/scripts/Messages/buildMessages.js
+++ src/scripts/Messages/buildMessages.js
@@ -7,39 +7,39 @@ const editMess = require("./editMessage")
 const addMessFriend = require("./addFriendMessage")
 // const currentUser = require("./currentUser")
 let currentUse = sessionStorage.getItem("User")
-console.log(currentUse) 
+console.log(currentUse)
 
 
 //This function creates an article where all of the message functionality goes, such as the messages, and the new message input field. It then executes an ajax call to the list of messages and runs the functions to build each message with a functional edit button.
 const buildMessageArticle = function () {
-    
+
     const $messageArticle = document.createElement("article")
     $messageArticle.id = "message-article";
-      
+
     //This part creates the article for messages, the input box, and appends them to the DOM
     $('<section>').attr('id', 'message-box').appendTo($messageArticle);
     let $inputDiv = $("<div>").attr("id", "input-div")
     $('<input>').attr('type', 'text').attr('id', 'message-input').appendTo($inputDiv);
     $("<button>").attr('type', 'button').attr('id', 'message-btn').text("Send Message").appendTo($inputDiv);
     $inputDiv.appendTo($messageArticle)
-    
+
     $("#messages").append($messageArticle)
-    
+
     //this calls the function that adds the event listener to the submit button
     subMess();
-    
+
     //this executes an ajax call to the api for all of the messages, and then builds message components with an edit function for each.
     ajax.getField("messages").then(function (messageList) {
-        
+
         //ajax call returns an array that is then looped through to build each message
         messageList.forEach(element => {
             let mess = document.createElement("p")
-            
+
             //this ajax call gets the userId of the message, which is used to give proper ids to the elements, and will be used to check for current user later on.
             ajax.getUser(element.userId).then(function (response) {
 
                 let userName = response.name
-                mess.textContent = `${element.message}` 
+                mess.textContent = `${element.message}`
                 if (element.userId == currentUse) {
                     mess.classList = `message`
                     // mess.id = `${response.id}`
@@ -50,18 +50,19 @@ const buildMessageArticle = function () {
                     // mess.id = `${response.id}`
                 }
                 mess.id = `${element.id}`
-                
-                const scrollBottom = require("./scrollBottom") 
+
+                const scrollBottom = require("./scrollBottom")
                 scrollBottom();
 
-                
+
                 $("#message-box").append(mess)
-                let nameBtn = document.createElement("p") 
-                nameBtn.className = "nameBtn"  
-                nameBtn.textContent = `${response.name}: `    
-                mess.prepend(nameBtn)      
+                let nameBtn = document.createElement("p")
+                nameBtn.className = "nameBtn"
+                nameBtn.id = `${response.id}`
+                nameBtn.textContent = `${response.name}: `
+                mess.prepend(nameBtn)
                 addMessFriend(nameBtn);
- 
+
             })
         }
         )

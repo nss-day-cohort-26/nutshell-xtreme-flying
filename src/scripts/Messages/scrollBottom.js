bc941712633b0a0d75f5180000117fda84aec882
William Kimball <wkkimball043@gmail.com>
2018-07-06 11:38:44 -0500

    worked on scrolling issues

--- src/scripts/Messages/scrollBottom.js
+++ src/scripts/Messages/scrollBottom.js
@@ -1,10 +1,11 @@
 const $ = require("jquery"); 
 
-const scrollBottom = function () {  
-    $("#message-box").scrollTop = $("#message-box").scrollHeight  
-    // $("#message-input").focus(); 
+const scrollBottom = function () {   
+    let scrollHeight = $("#message-article").prop("scrollHeight")
+    scrollHeight = scrollHeight + 500
+    console.log(typeof scrollHeight)       
+    $("#message-article").scrollTop(999999999);           
+    console.log(typeof $("#message-article").scrollTop())           
 } 
-    
-
-scrollBottom(); 
+scrollBottom();    
 module.exports = scrollBottom; 

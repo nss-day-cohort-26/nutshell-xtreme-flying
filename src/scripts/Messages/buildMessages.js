ba316b0fe7e2911f2a6fd95615565b2e0aec70e1
William Kimball <wkkimball043@gmail.com>
2018-07-06 13:39:10 -0500

    im a bug-fixin

--- src/scripts/Messages/buildMessages.js
+++ src/scripts/Messages/buildMessages.js
@@ -6,8 +6,7 @@ const subMess = require("./submitMessage")
 const editMess = require("./editMessage")
 const addMessFriend = require("./addFriendMessage")
 // const currentUser = require("./currentUser")
-let currentUse = sessionStorage.getItem("User")
-console.log(currentUse) 
+
 
 
 //This function creates an article where all of the message functionality goes, such as the messages, and the new message input field. It then executes an ajax call to the list of messages and runs the functions to build each message with a functional edit button.
@@ -40,6 +39,8 @@ const buildMessageArticle = function () {
 
                 let userName = response.name
                 mess.textContent = `${element.message}` 
+                let currentUse = sessionStorage.getItem("User")
+                console.log(currentUse) 
                 if (element.userId == currentUse) {
                     mess.classList = `message`
                     // mess.id = `${response.id}`

3847773baefa2a88f5d8abfd8b73cc5cd5e73207
Jacob Henderson <element25668@gmail.com>
2018-07-09 12:27:53 -0500

    changed to date format with moment, looks more formal.

--- src/scripts/News/buildNews.js
+++ src/scripts/News/buildNews.js
@@ -1,6 +1,7 @@
 const $ = require("jquery");
-const ajax = require("../ajaxCalls")
-const events = require("./newsMaster")
+const ajax = require("../ajaxCalls");
+const events = require("./newsMaster");
+const moment = require("moment");
 const newsDomMethods = Object.create({}, {
 buildNews: {
     value: () => {
@@ -19,7 +20,7 @@ buildNews: {
             //Need event handler to add to database and clear & reload DOM
             //When adding info into fields, click create and it puts it into the database and refreshes DOM
             const $createButton = $("<button>").text("Create").addClass("btn btn-info").appendTo("#mainNewsSec").click(function(){
-                const fullTime = events.getDate()
+                const fullTime = new moment().format("YYYY-MM-DDThh:mm:ss");
                 const currentUser = sessionStorage.getItem("User")
                 ajax.postNews(currentUser, $titleInput.val(), $urlInput.val(), $descInput.val(), fullTime)
                 .then(item => {
@@ -28,24 +29,28 @@ buildNews: {
             })
         })
             const getArticles = () => {
-                ajax.getField("news?_expand=user")
-                .then(item => {
                     ajax.allFriends()
                     .then(friendsList => {
                         friendsList.push(sessionStorage.getItem("User"));
+                ajax.getField("news?_expand=user")
+                .then(item => {
                         // console.log("friendslist", friendsList);
                     //This is sorting each article by newest first
                     item.sort(function(a,b){
-                        return new Date(b.timestamp) - new Date(a.timestamp);
+                        // console.log("testing dates", new Date(a.timestamp).getTime(), new Date(b.timestamp).getTime());
+                        
+                            return new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime();
                         });
                         //This is getting each article and posting it to the DOM.
                     item.forEach(key => {
                         if(friendsList.includes(key.userId)){
+                        // console.log(moment(key.timestamp));
+                        const niceTime = moment(key.timestamp).format("dddd, MMMM Do YYYY, h:mm:ss a")
                         const $holderSec = $("<section>").addClass("articles").attr("id", key.id);
                         const $title = $("<h3>").text(key.title).appendTo($holderSec);
                         const $url = $("<p>").text(`Source: ${key.url}`).appendTo($holderSec);
                         const $desc = $("<p>").text(`Description: ${key.synopsis}`).appendTo($holderSec);
-                        const $timestamp = $("<p>").text(key.timestamp).appendTo($holderSec);
+                        const $timestamp = $("<p>").text(niceTime).appendTo($holderSec);
                         let $creatorName = $("<p>").text("").appendTo($holderSec);
                         if(key.userId === sessionStorage.getItem("User")){
                             $creatorName.text("You");
